f2f6a927bf10e7e102f70bb1eff290891f498d48
Polina <collegepolinagutova@gmail.com>
2026-04-08 15:07:19 +0700

    workcomeon

--- src/cli.js
+++ src/cli.js
@@ -1,10 +1,14 @@
-import readlineSync from 'readline-sync'
+import readlineSync from 'readline-sync';
 
 const getUserName = () => {
-  console.log('Welcome to the Brain Games!')
-  const name = readlineSync.question('May I have your name? ')
-  console.log(`Hello, ${name}!`)
-  return name
-}
+  console.log('Welcome to the Brain Games!');
+  const name = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${name}!`);
+  return name;
+};
 
-export default getUserName
+const getRandomNumber = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+export { getUserName, getRandomNumber };
